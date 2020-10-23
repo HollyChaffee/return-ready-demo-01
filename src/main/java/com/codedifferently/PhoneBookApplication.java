00af76e9d3a9cee469789a70e8d3f75fde87fa95
HollyChaffee <saltdc95@gmail.com>
2020-10-23 00:37:58 +0000

    my first check in

--- src/main/java/com/codedifferently/PhoneBookApplication.java
+++ src/main/java/com/codedifferently/PhoneBookApplication.java
@@ -1,7 +1,14 @@
 package com.codedifferently;
 
+import java.util.Scanner;
 
 public class PhoneBookApplication {
+    public static void main(String[] args){
+        Scanner fred = new Scanner(System.in);
+        System.out.println("Whats' your name?");
+       String name = fred.next();
+       System.out.println("Your name is " + name);
 
+    }
 
 }
